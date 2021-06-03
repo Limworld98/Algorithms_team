3a53facaec90fde893ae604dc68207808b689a61
Dongw1126 <topblade71@naver.com>
2021-06-03 15:54:43 +0900

    청소 왼료 표시도 추가

--- Project1/robot.h
+++ Project1/robot.h
@@ -5,6 +5,7 @@
 
 using namespace std;
 
+// 아래, 위, 오른쪽, 왼쪽
 int dx[4] = { 1,-1,0,0 };
 int dy[4] = { 0,0,1,-1 };
 
@@ -29,6 +30,7 @@ void robot::printMap(Map& map)
     const string a = "■";  //벽
     const string b = "□";  //청소하지 않은공간
     const string c = "◆"; //로봇청소기
+    const string d = "▦"; // 청소 완료
 
     for (int i = 0; i < map.width; i++)
     {
@@ -44,6 +46,9 @@ void robot::printMap(Map& map)
                 break;
             case 2:
                 cout << c;
+                break;
+            case 3:
+                cout << d;
                 break;
             }
         }
