16d86eefb0c9f7a7747c223ad1c8989a2c50d264
Dongw1126 <topblade71@naver.com>
2021-06-06 00:39:21 +0900

    평면도 6개 추가

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,23 +1,11 @@
 #include "robot.h"
+#include "blueprint.h"
 #include <vector>
 using namespace std;
 
 int main()
 {
-	vector<vector<int>> my_map1 = {
-		{1,1,1,1,1,1,1,1,1,1},
-		{1,0,0,0,0,0,0,0,0,1},
-		{1,0,0,0,0,0,0,0,0,1},
-		{1,0,0,0,0,0,0,0,0,1},
-		{1,0,0,0,0,0,0,0,0,1},
-		{1,0,0,0,0,0,0,0,0,1},
-		{1,0,0,0,0,0,0,0,0,1},
-		{1,0,0,0,0,0,0,0,0,1},
-		{1,0,0,0,0,2,0,0,0,1},
-		{1,1,1,1,1,1,1,1,1,1}
-	};
-
-	Map m(my_map1);
+	Map m(blueprints[6]);
 	robot a;
 	a.randomMove(m, 1);
 }
